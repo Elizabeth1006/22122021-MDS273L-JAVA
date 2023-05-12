ae3772a3c40b2bf5fd9999157416195238e626f7
Elizabeth1006 <118045058+Elizabeth1006@users.noreply.github.com>
2023-05-12 17:32:06 +0530

    m

--- Lab4/Lab4.java
+++ Lab4/Lab4.java
@@ -19,7 +19,7 @@ public class Lab4 {
         System.out.println("Enter the amount to deposit:") ;
      double depamount=Double.parseDouble(scanner.nextLine());
         accBalance=accBalance+depamount;
-        System.out.println(accBalance);
+        System.out.println("Balance:"+accBalance);
     }
     public static void withdraw(){
         Scanner scanner = new Scanner(System.in);
@@ -29,11 +29,31 @@ public class Lab4 {
             System.out.println("Not sufficient amount..");  
         }else{
            accBalance-=withamount; 
-        System.out.println(accBalance);
+        System.out.println("Balance:"+accBalance);
         }
 
     }
     public static void transactions(){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of transactions: ");
+        int numTransactions = Integer.parseInt(scanner.nextLine());
+    
+        int transactionCount = 1;
+        while (transactionCount <= numTransactions) {
+            System.out.println("Transaction #" + transactionCount + " - Enter D for deposit or W for withdrawal: ");
+            String transactionType = scanner.nextLine();
+            if (transactionType.equalsIgnoreCase("D")) {
+                deposit();
+            } else if (transactionType.equalsIgnoreCase("W")) {
+                withdraw();
+            } else {
+                System.out.println("Invalid ");
+                continue;
+            }
+    
+            transactionCount++;
+        }
 
     }
     public static void accountsummary(){
@@ -45,7 +65,7 @@ public class Lab4 {
         initializeCustomer(123,"Elizabeth",200000);
         
         // Scanner scanner=new Scanner(System.in);
-        System.out.println("Enter the number");
+        System.out.println("Enter the choice:\n1-Deposit:\n2-Withdraw:\n3-Transactions:\n4-Account Summary:");
         int option=scanner.nextInt();
         
         
